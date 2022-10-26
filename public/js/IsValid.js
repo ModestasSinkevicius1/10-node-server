8e0dd93af5612331500ad1878af6a8b61dfff474
ModestasSinkevicius1 <modestas.sinkevicius1@gmail.com>
2022-10-26 11:21:53 +0300

    updated validation

--- public/js/IsValid.js
+++ public/js/IsValid.js
@@ -1,21 +1,27 @@
 class IsValid {
     static username(str) {
         if(typeof str !== 'string' || str.length < 2 || str.length > 30) {
-            return [true, 'Not good'];
+            return [true, 'Username either too short or too long'];
+        }
+        if(str !== str.trim()){
+            return [true, 'Username should not have outside whitespaces'];
         }
         return [false, 'OK'];
     }
 
     static email(str) {
-        if(typeof str !== 'string' || str.length < 2 || str.length > 30) {
-            return [true, 'Not good'];
+        if(typeof str !== 'string' || str.length < 2 || str.length > 100) {
+            return [true, 'Email too short or too long'];
+        }
+        if(str !== str.trim()){
+            return [true, 'Email should not have outside whitespaces'];
         }
         return [false, 'OK'];
     }
 
     static password(str) {
-        if(typeof str !== 'string' || str.length < 2 || str.length > 30) {
-            return [true, 'Not good'];
+        if(typeof str !== 'string' || str.length < 2 || str.length > 50) {
+            return [true, 'Password too short or too long'];
         }
         return [false, 'OK'];
     }
