6911ac6201f837616ceeeee2f65c945d68edd6f6
ModestasSinkevicius1 <modestas.sinkevicius1@gmail.com>
2022-10-27 15:22:31 +0300

    added validations and user  data saving

--- api/register.js
+++ api/register.js
@@ -1,9 +1,12 @@
+import { file } from "../lib/file.js";
+import { IsValid } from "../lib/IsValid.js";
+
 const handler = {};
 
-handler.init = (data, callback) => {
+handler.init = async (data, callback) => {
     const allowrdMethods = ['get', 'post', 'put', 'delete'];
     if(allowrdMethods.includes(data.httpMethod)) {
-        handler._handler[data.httpMethod](data);
+        return await handler._handler[data.httpMethod](data);
     }
     return 'Received http method is forbiden';
 }
@@ -15,9 +18,37 @@ handler._handler.get = (data) => {
     return 'GET API register';
 }
 
-handler._handler.post = (data) => {
-    console.log('POST register', data.payload);
-    return 'POST API register';
+handler._handler.post = async (data) => {
+
+    const { payload } = data;
+    const { username, email, password, repeatPassword } = payload;
+
+    delete payload.repeatPassword;
+
+    const [errUsername, msgUsername] = IsValid.username(username);
+    if(errUsername) {
+        return msgUsername;
+    }
+
+    const [errEmail, msgEmail] = IsValid.email(email);
+    if(errEmail) {
+        return msgEmail;
+    }
+
+    const [errPassword, msgPassword] = IsValid.password(password);
+    if(errPassword) {
+        return msgPassword;
+    }
+
+    if(password !== repeatPassword) {
+        return 'Passwords don\'t match';
+    }
+
+    const [error, msg] = await file.create('/users', email + '.json', payload);
+
+    
+
+    return error ? 'Failed to create account' : msg;
 }
 
 handler._handler.put = (data) => {
